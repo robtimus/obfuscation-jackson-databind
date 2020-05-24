44fb02b029640d89fedc99d1198bdfe5fe835bfc
Rob Spoor <rob@icemanx.nl>
2020-05-24 17:08:19 +0200

    Replaced automatic module name with a module descriptor; use fixed obfuscation-annotations version

---
+++ src/moditect/module-info.java
@@ -0,0 +1,7 @@
+module com.github.robtimus.obfuscation.jackson.databind {
+    requires com.github.robtimus.obfuscation;
+    requires com.github.robtimus.obfuscation.annotations;
+    requires com.fasterxml.jackson.databind;
+
+    exports com.github.robtimus.obfuscation.jackson.databind;
+}
